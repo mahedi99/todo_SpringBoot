a8dcb95f7cc09af64e71919917a7eff0876ad4b0
mahedi99 <mahedi9000@gmail.com>
2020-04-27 04:08:05 -0400

    route guard added

--- todo/src/app/app-routing.module.ts
+++ todo/src/app/app-routing.module.ts
@@ -5,14 +5,15 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
 import { LogoutComponent } from './logout/logout.component';
+import { RouteguardService } from './service/routeguard.service';
 
 
 const routes: Routes = [
   {path:'', component: LoginComponent},
   {path:'login', component: LoginComponent},
-  {path:'welcome/:name', component: WelcomeComponent},
-  {path: 'todos', component: ListTodosComponent},
-  {path: 'logout', component: LogoutComponent},
+  {path:'welcome/:name', component: WelcomeComponent, canActivate: [RouteguardService]},
+  {path: 'todos', component: ListTodosComponent, canActivate: [RouteguardService]},
+  {path: 'logout', component: LogoutComponent, canActivate: [RouteguardService]},
   {path:'**', component: ErrorComponent}
 ];
 
---
+++ todo/src/app/service/routeguard.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { HardcodedAuthService } from './hardcoded-auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RouteguardService implements CanActivate {
+
+  constructor(private hardcodedAuthService : HardcodedAuthService, private router: Router) { }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if(this.hardcodedAuthService.isUserLoggedIn()){
+      return true
+    }
+    else {
+      this.router.navigate((['login']))
+     return false;
+    }
+  }
+}
