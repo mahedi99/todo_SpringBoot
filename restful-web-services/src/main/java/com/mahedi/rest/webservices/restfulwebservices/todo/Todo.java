cb51c7c4bfa212eeebb40dcc546bac9060ed6a44
mahedi99 <mahedi9000@gmail.com>
2020-05-19 02:55:45 -0400

    added lombok

--- restful-web-services/src/main/java/com/mahedi/rest/webservices/restfulwebservices/todo/Todo.java
+++ restful-web-services/src/main/java/com/mahedi/rest/webservices/restfulwebservices/todo/Todo.java
@@ -1,5 +1,10 @@
 package com.mahedi.rest.webservices.restfulwebservices.todo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -12,6 +17,8 @@ import java.util.Objects;
  */
 
 @Entity
+@Getter @Setter @NoArgsConstructor
+@AllArgsConstructor
 public class Todo {
 
     @Id
@@ -22,67 +29,4 @@ public class Todo {
     private String description;
     private Date targetDate;
     private boolean isDone;
-
-    protected Todo(){}
-
-    public Todo(Long id, String username, String description, Date targetDate, boolean isDone) {
-        this.id = id;
-        this.username = username;
-        this.description = description;
-        this.targetDate = targetDate;
-        this.isDone = isDone;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String userName) {
-        this.username = userName;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Date getTargetDate() {
-        return targetDate;
-    }
-
-    public void setTargetDate(Date targetDate) {
-        this.targetDate = targetDate;
-    }
-
-    public boolean isDone() {
-        return isDone;
-    }
-
-    public void setDone(boolean done) {
-        isDone = done;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Todo todo = (Todo) o;
-        return id == todo.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
